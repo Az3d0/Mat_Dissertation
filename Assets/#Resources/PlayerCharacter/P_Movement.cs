52b56413c4beb1503af4bd3f061e4af230f4af6a
Az3d0 <szmate158@gmail.com>
2025-09-19 02:19:26 +0100

    Small Changes

--- Assets/#Resources/PlayerCharacter/P_Movement.cs
+++ Assets/#Resources/PlayerCharacter/P_Movement.cs
@@ -2,22 +2,31 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class P_Movement : PlayerCharacter
 {
+
+    [SerializeField] private float  m_speed = 1;
     private Vector3 m_playerMovementDirection;
     private bool m_isMoving;
 
+    private Rigidbody m_Rigidbody;
     protected override void Awake()
     {
-        base.Awake ();
+        base.Awake();
 
         m_inputActions.Player.Move.performed += OnMoveAction;
         m_inputActions.Player.Move.canceled += OnMoveAction;
+
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        
+        if(m_isMoving)
+        {
+            m_Rigidbody.AddForce(m_playerMovementDirection * m_speed * 0.01f, ForceMode.Force);
+        }
     }
 
     private void OnMoveAction(InputAction.CallbackContext context)
@@ -25,11 +34,16 @@ public class P_Movement : PlayerCharacter
         HandleMovement(context.ReadValue<Vector2>());
     }
 
+
     private void HandleMovement(Vector2 movementInput)
     {
         m_playerMovementDirection = new Vector3(movementInput.x, 0, movementInput.y);
 
         m_isMoving = movementInput.x != 0 || movementInput.y != 0;
+
+        Debug.Log(m_isMoving);
+
         //m_animator.SetBool("isWalking", IsMoving);
+
     }
 }
